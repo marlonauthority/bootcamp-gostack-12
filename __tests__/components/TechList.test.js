7913a9d2e5fe1d919b86e5bf8c4d6655c3116412
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-26 14:15:18 -0400

    formulario

--- __tests__/components/TechList.test.js
+++ __tests__/components/TechList.test.js
@@ -4,10 +4,13 @@ import TechList from "~/components/TechList";
 
 describe("TechList component", () => {
   it("should be able to add new tech", () => {
-    const { getByText, getByTestId, debug } = render(<TechList />);
-    debug();
-    fireEvent.click(getByText("Adicionar"));
-    debug();
+    const { getByText, getByTestId, debug, getByLabelText } = render(<TechList />);
+    //debug();
+    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js'} })
+    fireEvent.submit(getByTestId('tech-form'))
+    //fireEvent.click(getByText("Adicionar"));
+    //debug();
     expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
+    expect(getByLabelText('Tech')).toHaveValue('');
   });
 });
